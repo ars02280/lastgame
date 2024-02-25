04ece68dd2c0d102776f56558824ef789dc39395
ars02280 <spakspak01@gmail.com>
2024-02-25 14:49:00 +0600

    fireball

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value = 100;
+    void Start()
+    {
+        
+    }
+    
+
+    void Update()
+    {
+        
+    }
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/FireBalll.cs
+++ Assets/Scripts/FireBalll.cs
@@ -6,24 +6,36 @@ public class FireBalll : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
+    public float damage = 10;
 
     void Start()
     {
         Invoke("DestroyFireBall", lifeTime);
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireBall();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position += transform.forward * speed * Time.fixedDeltaTime;
         
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        DestroyFireBall();
-    }
     private void DestroyFireBall()
     {
         Destroy(gameObject);
     }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        } 
+    }
+    
 }
